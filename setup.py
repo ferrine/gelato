ea5855a7248dfc0e9823624f08af8f94f2f053d6
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-26 21:51:10 +0300

    added requirements

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+REQUIREMENTS = [
+    #'theano',
+    'lasagne',
+    #'pymc3'
+]
+
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+if __name__ == '__main__':
+    setup(
+        name='gelato',
+        packages=find_packages(),
+        description='Bayesian desert for Lasagne',
+        long_description=LONG_DESCRIPTION,
+        author='Maxim Kochurov',
+        author_email='maxim.v.kochurov@gmail.com',
+        download_url='https://github.com/ferrine/gelato',
+        install_requires=REQUIREMENTS,
+        tests_require=['pep8', 'pylint', 'coverage']
+    )
