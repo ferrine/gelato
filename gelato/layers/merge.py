8bed5321efc5a164348a9fc8be258a6602c77d96
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-30 15:54:38 +0300

    added more layers

---
+++ gelato/layers/merge.py
@@ -0,0 +1,11 @@
+import sys
+from lasagne.layers.merge import __all__
+import lasagne.layers.merge as __cloned
+from .base import bayes as __bayes
+__module = sys.modules[__name__]
+del sys
+for obj_name in __all__:
+    try:
+        setattr(__module, obj_name, __bayes(getattr(__cloned, obj_name)))
+    except TypeError:
+        setattr(__module, obj_name, getattr(__cloned, obj_name))
---
+++ gelato/layers/shape.py
@@ -0,0 +1,12 @@
+import sys
+from lasagne.layers.embedding import __all__
+import lasagne.layers.embedding as __cloned
+from .base import bayes as __bayes
+__module = sys.modules[__name__]
+del sys
+for obj_name in __all__:
+    try:
+        setattr(__module, obj_name, __bayes(getattr(__cloned, obj_name)))
+    except TypeError:
+        setattr(__module, obj_name, getattr(__cloned, obj_name))
+м
