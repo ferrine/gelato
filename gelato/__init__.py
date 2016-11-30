0d8acfbfe5852b13433ba4e035f113a4f40b5e87
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-30 20:13:23 +0300

    Added pymc3 version check

--- gelato/__init__.py
+++ gelato/__init__.py
@@ -1,5 +1,10 @@
-from . import spec
-from . import random
-from . import variational
-from . import layers
-from .version import __version__
+import pymc3
+if not hasattr(pymc3.Model, 'root'):
+    raise ImportError('Need pymc3>=3.1')
+else:
+    del pymc3
+    from . import spec
+    from . import random
+    from . import variational
+    from . import layers
+    from .version import __version__
