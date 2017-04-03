ab365c8eacebdbdf8cf803f2588621d3d7ad79da
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-04-03 18:28:22 +0300

    fixes in stats layers

--- gelato/layers/stats.py
+++ gelato/layers/stats.py
@@ -1,14 +1,18 @@
 from .base import Layer
 
 
-class ReplaceLayer(Layer):
+class PosteriorLayer(Layer):
     def get_output_for(self, incoming, approx=None, deterministic=False, **kwargs):
         return approx.apply_replacements(incoming, deterministic=deterministic)
 
 
 class SamplingLayer(Layer):
-    def get_output_for(self, incoming, approx=None, samples=10, **kwargs):
-        return approx.sample_node(incoming, samples=samples)
+    def __init__(self, incoming, samples=10, name=None):
+        super(SamplingLayer, self).__init__(incoming, name=name)
+        self.samples = samples
+
+    def get_output_for(self, incoming, approx=None, **kwargs):
+        return approx.sample_node(incoming, size=self.samples)
 
     def get_output_shape_for(self, input_shape):
         return (None, ) + input_shape
