378a74b09282d1139bdc2558c646380a34f012a8
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-06-24 20:54:12 +0300

    add random module
    (cherry picked from commit 38ff784)

--- gelato/__init__.py
+++ gelato/__init__.py
@@ -1,4 +1,5 @@
-from pymc3.theanof import set_tt_rng, tt_rng
+from . import random
+from .random import *
 from gelato.specs import *
 from . import layers
 from . import specs
---
+++ gelato/random.py
@@ -0,0 +1,5 @@
+from pymc3.theanof import set_tt_rng, tt_rng
+__all__ = [
+    'set_tt_rng',
+    'tt_rng'
+]
